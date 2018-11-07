cd5be0e2af9e92a56c8f94250e576560f54b2cb6
Costa82 <costa_torito@hotmail.com>
2018-11-07 19:54:59 +0100

    Merienda infantil

--- Merendalia/jquery/ocultar_productos.js
+++ Merendalia/jquery/ocultar_productos.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 	        $('.refrescos')[ ($(this).val() == 'refrescos' || $(this).val() == 'todo' || $(this).val() == 'bebidas') ? 'show' : 'hide' ]()
 	        $('.cervezas')[ ($(this).val() == 'cervezas' || $(this).val() == 'todo' || $(this).val() == 'bebidas') ? 'show' : 'hide' ]()
 	        $('.vinos')[ ($(this).val() == 'vinos' || $(this).val() == 'todo' || $(this).val() == 'bebidas') ? 'show' : 'hide' ]()
-	        $('.titulo_bebida')[ ($(this).val() != 'comida') ? 'show' : 'hide' ]()
+	        $('.titulo_bebida')[ ($(this).val() != 'comida' && $(this).val() != 'merienda') ? 'show' : 'hide' ]()
 	        $('.comida')[ ($(this).val() == 'comida' || $(this).val() == 'todo') ? 'show' : 'hide' ]()
+	        $('.merienda')[ ($(this).val() == 'merienda' || $(this).val() == 'todo') ? 'show' : 'hide' ]()
         });
 });
