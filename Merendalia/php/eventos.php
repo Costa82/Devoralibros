97b33e5c46470a7ded58d6243e68370f097f9b22
Costa82 <costa_torito@hotmail.com>
2019-01-29 20:31:46 +0100

    Update eventos.php
    modificar una errata

--- Merendalia/php/eventos.php
+++ Merendalia/php/eventos.php
@@ -93,7 +93,7 @@
 			<p>Os traemos el evento <strong>MENÚ SAN VALENTÍN</strong> para que celebréis con vuestra pareja, amigos, o con quien más rabia
 				os de, el día de San Valentín, ¡porque de lo que se trata es de juntarse y celebrar :-P!</p>
 					<br>
-					<p>El evento está se celebrará los días 14, 15, 16 y 17 de febrero, tanto a la comida, a partir de las 14:00 horas, como a la cena, 
+					<p>El evento se celebrará los días 14, 15, 16 y 17 de febrero, tanto a la comida, a partir de las 14:00 horas, como a la cena, 
 					a partir de las 21:00.</p>
 					<br>
 					<p><strong>Por 20€ por persona</strong> disfrutaréis de un menú pensado para que disfrutéis comiendo en pareja (o no), compartiendo los 
