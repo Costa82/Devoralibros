69de87623b9faa811d8df691ea4a0a593a76d81c
Costa82 <costa_torito@hotmail.com>
2019-02-16 10:59:20 +0100

    Eventos pasados comentados

--- Merendalia/php/eventos.php
+++ Merendalia/php/eventos.php
@@ -116,7 +116,7 @@
 
 		</div>
 
-<div class="contenedor_eventos">
+		<div class="contenedor_eventos">
 
 		<div class="evento_principal">
 			<h3>TALLERES INFANTILES EN MERENDALIA</h3>
@@ -143,10 +143,12 @@
 				</br>
 				</br>
 
-			<img src='../img/Talleres_infantiles_Enero_Alargada.jpg'
-				title='Talleres Infantiles' class="img_movil" /> <img
-				src='../img/Talleres_infantiles_Enero_apaisado.jpg'
-				title='Talleres Infantiles' class="img_ordenador" />
+			<!-- IMAGEN TALLER ENERO -->
+
+<!-- 			<img src='../img/Talleres_infantiles_Enero_Alargada.jpg' -->
+<!-- 				title='Talleres Infantiles' class="img_movil" /> <img -->
+<!-- 				src='../img/Talleres_infantiles_Enero_apaisado.jpg' -->
+<!-- 				title='Talleres Infantiles' class="img_ordenador" /> -->
 
 		</div>
 
@@ -154,159 +156,161 @@
 
 	<!-- EVENTOS SECUNDARIOS -->
 
-	<h3 class="eventos_pasados">EVENTOS PASADOS</h3>
+<!-- 	<h3 class="eventos_pasados">EVENTOS PASADOS</h3> -->
 
-	<div class="contenedor_eventos_secundarios">
+<!-- 	<div class="contenedor_eventos_secundarios"> -->
 	
-		<div class="evento_secundario">
+<!-- 		<div class="evento_secundario"> -->
 
-			<h3>MERENDALIA Y MANAI</h3>
+<!-- 			<h3>MERENDALIA Y MANAI</h3> -->
 
-			<div class="explicacion_evento">
-				<p></p>
-				</br>
-			</div>
-
-			<!-- img para móviles -->
-			<img src='../img/MerendaliaYManai_Alargada.jpeg'
-				title='Merendalia y Manai' class="img_movil" />
-			<!-- img con zoom para el resto de dispositivos -->
-			<a class="fancybox" rel="group"
-				href="../img/MerendaliaYManai_Apaisada.jpeg"><img
-				src='../img/MerendaliaYManai_Apaisada.jpeg'
-				title='Merendalia y Manai' class="img_ordenador" atl='Merendalia y Manai' /></a>
-
-		</div>
-
-		<div class="evento_secundario">
-
-			<h3>JORNADAS DE LA FABADA</h3>
-
-			<div class="explicacion_evento">
-				<p>
-					Esta vez en Merendalia vamos a disfrutar de los placeres de la
-					comida de otoño, no os perdáis nuestra riquísima fabada, todos los
-					fines de semana de noviembre a la hora de comer.</br>
-					</br> Llámanos al 983.85.73.69 y reserva para todos los que seáis,
-					¡os chuparéis los dedos!</br>
-					</br> (Recuerda también que hay menús para niños)
-				</p>
-				</br>
-			</div>
+<!-- 			<div class="explicacion_evento"> -->
+<!-- 				<p></p> -->
+<!-- 				</br> -->
+<!-- 			</div> -->
 
 			<!-- img para móviles -->
-			<img src='../img/Alargada_Cartel_Jornadas_Fabada.png'
-				title='Jornadas Fabada' class="img_movil" />
+<!-- 			<img src='../img/MerendaliaYManai_Alargada.jpeg' -->
+<!-- 				title='Merendalia y Manai' class="img_movil" /> -->
 			<!-- img con zoom para el resto de dispositivos -->
-			<a class="fancybox" rel="group"
-				href="../img/Apaisaada_Cartel_Jornadas_Fabada.png"><img
-				src="../img/Apaisaada_Cartel_Jornadas_Fabada.png"
-				class="img_ordenador" title='Jornadas Fabada' alt="Jornadas Fabada" /></a>
+<!-- 			<a class="fancybox" rel="group" -->
+<!-- 				href="../img/MerendaliaYManai_Apaisada.jpeg"><img -->
+<!-- 				src='../img/MerendaliaYManai_Apaisada.jpeg' -->
+<!-- 				title='Merendalia y Manai' class="img_ordenador" atl='Merendalia y Manai' /></a> -->
 
-		</div>
+<!-- 		</div> -->
 
-		<div class="evento_secundario">
+<!-- 		<div class="evento_secundario"> -->
 
-			<h3>TIEMPO DE VENDIMIA</h3>
+<!-- 			<h3>JORNADAS DE LA FABADA</h3> -->
 
-			<div class="explicacion_evento">
-				<p>Os traemos el evento Tiempo de Vendimia. Con él hemos querido
-					hacer homenaje a la vendimia y a las cinco denominaciones de origen
-					de vino que tenemos en la provincia de Valladolid.</p>
-				</br>
-				<p>
-					D.O. Rueda, D.O. Toro, D.O. Ribera del Duero, D.O. Cigales y D.O.
-					Tierra de León (sí, aunque no os lo creáis, parte de esa
-					denominación se da dentro de la provincia de Valladolid, haz click
-					<a
-						href="https://www.xn--vino-espaa-19a.es/Denominacion-de-Origen-Tierra-de-Leon.html"
-						class="seleccionado">aquí</a> si quieres leer más sobre ello).
-				</p>
+<!-- 			<div class="explicacion_evento"> -->
+<!-- 				<p> -->
+<!-- 					Esta vez en Merendalia vamos a disfrutar de los placeres de la -->
+<!-- 					comida de otoño, no os perdáis nuestra riquísima fabada, todos los -->
+<!-- 					fines de semana de noviembre a la hora de comer.</br> -->
+<!-- 					</br> Llámanos al 983.85.73.69 y reserva para todos los que seáis, -->
+<!-- 					¡os chuparéis los dedos!</br> -->
+<!-- 					</br> (Recuerda también que hay menús para niños) -->
+<!-- 				</p> -->
+<!-- 				</br> -->
+<!-- 			</div> -->
+
+			<!-- img para móviles -->
+<!-- 			<img src='../img/Alargada_Cartel_Jornadas_Fabada.png' -->
+<!-- 				title='Jornadas Fabada' class="img_movil" /> -->
+			<!-- img con zoom para el resto de dispositivos -->
+<!-- 			<a class="fancybox" rel="group" -->
+<!-- 				href="../img/Apaisaada_Cartel_Jornadas_Fabada.png"><img -->
+<!-- 				src="../img/Apaisaada_Cartel_Jornadas_Fabada.png" -->
+<!-- 				class="img_ordenador" title='Jornadas Fabada' alt="Jornadas Fabada" /></a> -->
+
+<!-- 		</div> -->
+
+<!-- 		<div class="evento_secundario"> -->
+
+<!-- 			<h3>TIEMPO DE VENDIMIA</h3> -->
+
+<!-- 			<div class="explicacion_evento"> -->
+<!-- 				<p>Os traemos el evento Tiempo de Vendimia. Con él hemos querido -->
+<!-- 					hacer homenaje a la vendimia y a las cinco denominaciones de origen -->
+<!-- 					de vino que tenemos en la provincia de Valladolid.</p> -->
+<!-- 				</br> -->
+<!-- 				<p> -->
+<!-- 					D.O. Rueda, D.O. Toro, D.O. Ribera del Duero, D.O. Cigales y D.O. -->
+<!-- 					Tierra de León (sí, aunque no os lo creáis, parte de esa -->
+<!-- 					denominación se da dentro de la provincia de Valladolid, haz click -->
+<!-- 					<a -->
+<!-- 						href="https://www.xn--vino-espaa-19a.es/Denominacion-de-Origen-Tierra-de-Leon.html" -->
+<!-- 						class="seleccionado">aquí</a> si quieres leer más sobre ello). -->
+<!-- 				</p> -->
 				<!-- </br>
-    				<p>El menú que os presentamos es un menú degustación elaborado en
-    					base a cada una de las denominaciones de origen que se dan en la
-    					provincia de Valladolid. Cada uno de los cinco platos tiene como
-    					eje principal una de ellas y un producto, que serán el centro
-    					principal de cada uno. Además de los cinco platos, tendremos 5
-    					tipos de vino para que podáis maridar como os apetezca, uno de cada
-    					una de las denominaciones. Hacemos así una fiesta de la vendimia a
-    					nuestro estilo y homenajeamos en nuestra tierra a uno de los
-    					productos más importantes y consumidos de la misma.</p> -->
-				</br>
-				<p>De verdad, ¡¡esperamos que os guste!!</p>
-			</div>
+<!--     				<p>El menú que os presentamos es un menú degustación elaborado en -->
+<!--     					base a cada una de las denominaciones de origen que se dan en la -->
+<!--     					provincia de Valladolid. Cada uno de los cinco platos tiene como -->
+<!--     					eje principal una de ellas y un producto, que serán el centro -->
+<!--     					principal de cada uno. Además de los cinco platos, tendremos 5 -->
+<!--     					tipos de vino para que podáis maridar como os apetezca, uno de cada -->
+<!--     					una de las denominaciones. Hacemos así una fiesta de la vendimia a -->
+<!--     					nuestro estilo y homenajeamos en nuestra tierra a uno de los -->
+<!--     					productos más importantes y consumidos de la misma.</p> --> -->
+<!-- 				</br> -->
+<!-- 				<p>De verdad, ¡¡esperamos que os guste!!</p> -->
+<!-- 			</div> -->
 
 			<!-- img para móviles -->
-			<img src='../img/Alargado_Evento_Vendimia.jpg'
-				title='Tiempo de Vendimia' class="img_movil" />
+<!-- 			<img src='../img/Alargado_Evento_Vendimia.jpg' -->
+<!-- 				title='Tiempo de Vendimia' class="img_movil" /> -->
 			<!-- img con zoom para el resto de dispositivos -->
-			<a class="fancybox" rel="group"
-				href="../img/Apaisado_Evento_Vendimia.png"><img
-				src="../img/Apaisado_Evento_Vendimia.png" class="img_ordenador"
-				title='Tiempo de Vendimia' alt="Tiempo de Vendimia" /></a>
+<!-- 			<a class="fancybox" rel="group" -->
+<!-- 				href="../img/Apaisado_Evento_Vendimia.png"><img -->
+<!-- 				src="../img/Apaisado_Evento_Vendimia.png" class="img_ordenador" -->
+<!-- 				title='Tiempo de Vendimia' alt="Tiempo de Vendimia" /></a> -->
 
-		</div>
+<!-- 		</div> -->
 
-		<div class="evento_secundario">
+<!-- 		<div class="evento_secundario"> -->
 
-			<h3>OKTOBERFEST</h3>
+<!-- 			<h3>OKTOBERFEST</h3> -->
 
-			<div class="explicacion_evento">
-				<p>
-					Os traemos el evento Oktoberfest. Con él hemos querido crear un
-					menú alemán en base a esta fiesta tan famosa de Múnich que se
-					inició con una boda real en 1.810 (haz click <a
-						href="https://www.oktoberfest.net/historia-del-oktoberfest/"
-						class="seleccionado">aquí</a> si quieres leer más sobre ello).
-					Hacemos un recorrido por los platos más típicos Bávaros aderezados
-					con buenas cervezas de Múnich, todo ello en nuestro local decorado
-					especialmente para el evento, así como la música y el atrezo de los
-					camareros.
-				</p>
-				</br>
-				<p>¡Esperamos que disfrutéis!</p>
-			</div>
+<!-- 			<div class="explicacion_evento"> -->
+<!-- 				<p> -->
+<!-- 					Os traemos el evento Oktoberfest. Con él hemos querido crear un -->
+<!-- 					menú alemán en base a esta fiesta tan famosa de Múnich que se -->
+<!-- 					inició con una boda real en 1.810 (haz click <a -->
+<!-- 						href="https://www.oktoberfest.net/historia-del-oktoberfest/" -->
+<!-- 						class="seleccionado">aquí</a> si quieres leer más sobre ello). -->
+<!-- 					Hacemos un recorrido por los platos más típicos Bávaros aderezados -->
+<!-- 					con buenas cervezas de Múnich, todo ello en nuestro local decorado -->
+<!-- 					especialmente para el evento, así como la música y el atrezo de los -->
+<!-- 					camareros. -->
+<!-- 				</p> -->
+<!-- 				</br> -->
+<!-- 				<p>¡Esperamos que disfrutéis!</p> -->
+<!-- 			</div> -->
 
 			<!-- img para móviles -->
-			<img src='../img/Alargado_Evento_Oktoberfest.png' title='Oktoberfest'
-				class="img_movil" />
+<!-- 			<img src='../img/Alargado_Evento_Oktoberfest.png' title='Oktoberfest' -->
+<!-- 				class="img_movil" /> -->
 			<!-- img con zoom para el resto de dispositivos -->
-			<a class="fancybox" rel="group"
-				href="../img/Apaisado_Evento_Oktoberfest.png"><img
-				src="../img/Apaisado_Evento_Oktoberfest.png" class="img_ordenador"
-				title='Oktoberfest' alt="Oktoberfest" /></a>
+<!-- 			<a class="fancybox" rel="group" -->
+<!-- 				href="../img/Apaisado_Evento_Oktoberfest.png"><img -->
+<!-- 				src="../img/Apaisado_Evento_Oktoberfest.png" class="img_ordenador" -->
+<!-- 				title='Oktoberfest' alt="Oktoberfest" /></a> -->
 
-		</div>
+<!-- 		</div> -->
 
-		<div class="evento_secundario">
+<!-- 		<div class="evento_secundario"> -->
 
-			<h3>SIDRERÍA FERIAS</h3>
+<!-- 			<h3>SIDRERÍA FERIAS</h3> -->
 
-			<div class="explicacion_evento">
-				<p>Merendalia abre sus puertas en Valladolid. Para ello hemos
-					diseñado un evento basado en un Menú Sidrería para Ferias, donde
-					hemos querido evocar los ricos platos de las sidrerías vascas, y
-					darles un toque especial propio de Merendalia. La máxima en este
-					menú es el respeto al producto</p>
-				</br>
-				<p>¡Ven a compartir con nosotros el auténtico menú sidrería y
-					preocúpate solo de disfrutar!</p>
-			</div>
+<!-- 			<div class="explicacion_evento"> -->
+<!-- 				<p>Merendalia abre sus puertas en Valladolid. Para ello hemos -->
+<!-- 					diseñado un evento basado en un Menú Sidrería para Ferias, donde -->
+<!-- 					hemos querido evocar los ricos platos de las sidrerías vascas, y -->
+<!-- 					darles un toque especial propio de Merendalia. La máxima en este -->
+<!-- 					menú es el respeto al producto</p> -->
+<!-- 				</br> -->
+<!-- 				<p>¡Ven a compartir con nosotros el auténtico menú sidrería y -->
+<!-- 					preocúpate solo de disfrutar!</p> -->
+<!-- 			</div> -->
 
 			<!-- img para móviles -->
-			<img src='../img/Alargado_Evento_Sidrería_Ferias.png'
-				title='Menú Sidrería' class="img_movil" />
+<!-- 			<img src='../img/Alargado_Evento_Sidrería_Ferias.png' -->
+<!-- 				title='Menú Sidrería' class="img_movil" /> -->
 			<!-- img con zoom para el resto de dispositivos -->
-			<a class="fancybox" rel="group"
-				href="../img/Apaisado_Evento_Sidrería_Ferias.png"><img
-				src="../img/Apaisado_Evento_Sidrería_Ferias.png"
-				class="img_ordenador" title='Menú Sidrería' alt="Menú Sidrería" /></a>
+<!-- 			<a class="fancybox" rel="group" -->
+<!-- 				href="../img/Apaisado_Evento_Sidrería_Ferias.png"><img -->
+<!-- 				src="../img/Apaisado_Evento_Sidrería_Ferias.png" -->
+<!-- 				class="img_ordenador" title='Menú Sidrería' alt="Menú Sidrería" /></a> -->
 
 
-		</div>
+<!-- 		</div> -->
 
-	</div>
--->
+<!-- 	</div> -->
+	
+    <!-- FIN EVENTOS SECUNDARIOS -->
+    
 	<footer>
         <?php include_once("footer.php");?>        
     </footer>
