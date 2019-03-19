ca2774b928a101f8122c4ab16fe17f94f87a869a
Costa82 <costa_torito@hotmail.com>
2019-03-19 19:50:49 +0100

    Update blogMerendalia.php
    Qué son los menús privados

--- Merendalia/php/blogMerendalia.php
+++ Merendalia/php/blogMerendalia.php
@@ -85,8 +85,201 @@
 		</br>
 
 	</div>
+	
+	<div class="articulo" id="menús_privados">
 
-	<div class="articulo">
+		<!-- PARTE PRINCIPAL DEL ARTÍCULO CON IMAGEN GRANDE Y TEXTO -->
+
+		<h3 class="titulo_articulo">EXPERIENCIA MERENDALIA: ¿QUÉ SON LOS MENÚS PRIVADOS?</h3>
+
+		<div class="fechaArticulo">
+			<p>07 de marzo de 2019</p>
+		</div>
+
+		<img
+			src='../img/brindis_mesa_amigos_comida.jpg'
+			title='brindis mesa amigos comida merendalia'
+			alt='brindis mesa amigos comida merendalia'
+			class="img_articulo" />
+
+		<div class="explicacion_articulo">
+
+			<p>¡Buenos días, merendalios!</br></br>
+			Hoy queremos hablaros de los menús privados y más en especial de una nueva versión, los ¡¡menús bocaditos!!, de la que hablaremos más adelante…
+			Existen dos opciones de menú. En cualquiera de ellas se dispone de la totalidad del espacio y un servicio dedicado en exclusiva para tu grupo. 
+			</p></br>
+			
+			<span class="leerMas"><strong>leer más...</strong></span>
+
+			<!-- Esta parte del texto estará oculta hasta hacer click en 'leer más...' -->
+
+			<div class="texto_leerMas">
+
+				</br>
+				<p>
+					<strong><i>Menús "A mesa puesta":</strong></i>
+				</p>
+				</br>
+				<p>Los comensales se sientan a la mesa y se les sirve un menú completo, 
+				entrantes, principal, postre, etc…, dependiendo de la opción elegida. Máximo de comensales, 24.</p>
+				</br>
+				<p>
+					<strong><i>Menús "Bocaditos":</strong></i>
+				</p>
+				</br>
+				<p>Los comensales permanecen de píe, mientras se los sirve un menú a base de pinchos fríos, calientes
+				y dulces. Tartaletas, canapés, conos, brochetas, croquetas, cucharitas, vasitos y mucho más conforman
+				este tipo de menú. Tiene más capacidad que el menú a mesa puesta.</p>
+				</br>
+				<p>Cualquiera de los menús entre las opciones disponibles, se sirven para grupos de mínimo 12 personas.</p></br>
+					<p><i>¿Qué beneficios tienen estos menús privados?</i></p>
+				</br>
+				<p>
+					-	Ese día en la cocina solo se preparan los platos/bocaditos de vuestro menú.</br>
+					-	El personal de sala os atiende en exclusiva.</br>
+					-	Los aseos (adaptado y con cambiador de bebé) solo son usados por vuestro grupo.</br>
+					-	Los niños disfrutan del parque de bolas.</br>
+					-	Música y TV, están a vuestra disposición. Ver un partido durante la comida, proyectar un vídeo familiar o unas fotografías ya no será un problema.
+				</p>
+				</br>
+				<p>
+				Queremos que os sintáis como en casa. Vive la EXPERIENCIA MERENDALIA.</br>
+				</p>
+
+				<div class="contenedor_articulo_secundario">
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/chuleton.jpg"><img
+							src="../img/chuleton.jpg"
+							class="img_articulo_secundaria"
+							title="Chuletón del menú "A mesa puesta", sidrería"
+							alt="Chuletón del menú "A mesa puesta", sidrería" /></a>
+						<p class="pieFoto">Chuletón del menú "A mesa puesta", sidrería</p>
+
+					</div>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/bacalao al pil pil.png"><img
+							src="../img/bacalao al pil pil.png"
+							class="img_articulo_secundaria"
+							title="Bacalao Pil-Pil del menú "A mesa puesta", sidrería y otros"
+							alt="Bacalao Pil-Pil del menú "A mesa puesta", sidrería y otros" /></a>
+						<p class="pieFoto">Bacalao Pil-Pil del menú "A mesa puesta", sidrería y otros</p>
+
+					</div>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/vasitos aguacate y langostino.jpg"><img
+							src="../img/vasitos aguacate y langostino.jpg"
+							class="img_articulo_secundaria"
+							title="Vasitos de aguacate y langostino del menú "Bocaditos""
+							alt="Vasitos de aguacate y langostino del menú "Bocaditos"" /></a>
+						<p class="pieFoto">Vasitos de aguacate y langostino del menú "Bocaditos"</p>
+
+					</div>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+
+						<a class="fancybox" rel="group"
+							href="../img/gildas.jpg"><img
+							src="../img/gildas.jpg"
+							class="img_articulo_secundaria"
+							title="Gildas con Piparras del menú "Bocaditos""
+							alt="Gildas con Piparras del menú "Bocaditos"" /></a>
+						<p class="pieFoto">Gildas (aceituna, anchoa suprema cántabra y guindillas de Ibarra) del menú "Bocaditos"</p>
+
+					</div>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/magret de pato menú japonés.jpg"><img
+							src="../img/magret de pato menú japonés.jpg"
+							class="img_articulo_secundaria"
+							title="Magret de pato del menú Japonés "A mesa puesta""
+							alt="Magret de pato del menú Japonés "A mesa puesta"" /></a>
+						<p class="pieFoto">Magret de pato del menú Japonés "A mesa puesta""</p>
+
+					</div>
+
+					
+				</div>
+
+				<div class="explicacion_final">
+
+					<p>
+						<i><strong>A Mesa Puesta</i></strong>
+					</p>
+
+					</br>
+					<p>Vamos adaptando los menús a vuestros gustos o a los productos de temporada. De ahí que vayan cambiando. 
+					Si quieres que te preparemos algo en concreto, o alguien te ha recomendado algo que probó y ahora no lo 
+					encuentras entre nuestras opciones, pregúntanos.</br></br>
+					Nos gusta la cocina un montón, la nuestra y la de fuera, y por eso alguno de nuestros menús son internacionales.
+					El menú alemán dentro de la fiesta OktoberFest, es uno de los que más éxito ha tenido y por eso lo mantenemos 
+					desde octubre. El menú japonés, es más reciente. Sabemos que lo japo triunfa y a veces no tanto, por el pescado crudo. 
+					Este menú gusta a unos y sorprende a los otros.
+					Entre los nacionales destacar el menú sidrería, menú contundente, rico y festivo. Se sirve con sidra, con la ayuda de 
+					nuestros amigos “los isidrines”. Es un menú al que tenemos mucho cariño. Siempre nos gustó mucho visitar las sidrerías 
+					cuando viajábamos por el norte de España, y fue con ese menú con el que arrancamos esta aventura, en septiembre del año 
+					pasado, en las fiestas de Valladolid. Desde entonces son muchas las personas que lo han probado y uno de sus platos se ha 
+					convertido sin dudadarlo, en el plato estrella de merendalia. Hablamos del bacalao al pil pil.</br></br>
+					Nos gusta que se prueben varios platos, y por eso <strong>los menús son pequeñas degustaciones</strong>. Siempre hay unos cuantos, 
+					a compartir, para terminar con un principal. Españoles o internacionales, todos los menús, comparten el sabor. <strong>Las
+					preparaciones son caseras</strong> y eso se nota. Ponemos mucho empeño en <strong>buscar los mejores proveedores</strong> 
+					que nos den los mejores productos. Un ejemplo de esto es la paliza que le di a Jose, nuestro carnicero, con los codillos, 
+					en pleno diseño del menú Oktober. Probamos con los traseros. Con piel, sin piel. Ahora dámelos delanteros y sin piel.
+					Así hasta dar con la pieza que nos permitió elaborar el codillo perfecto, asado en su jugo al estilo Alemán. Un abrazo a Jose y Rocío!!
+
+					</p>
+					</br>
+					<p>
+						<i><strong>Bocaditos</i></strong>
+					</p>
+					</br>
+
+					<p>Esta segunda opción nace de vosotros. Sabemos que sentarse a la mesa es la mejor opción en muchas de vuestras 
+					reuniones, pero a veces, buscáis algo más informal. De pie, interactuando unos con otros y sin descuidar la cocina. 
+					Son como los ágapes de las bodas, un vino español, estar de pinchos, etc…, pero con todas las ventajas que ofrece merendalia.
+					Tapear por el centro es un planazo, pero no siempre es lo más cómodo. Los abrigos estorban, tienes que andar cambiando de bar 
+					y muchas veces no se sabe que hacer con los niños.</br></br>
+					El plan ideal para organizar un buen vermú de fin de semana.</br></br>
+					Disponemos de tres menús diferentes a base de pinchos fríos, calientes y dulces. Vino, cerveza, agua o refresco incluido. </br></br></br></br>
+					Sea a mesa puesta o de bocaditos, sabemos que <strong>somos una de las mejores opciones de Valladolid para tus reuniones.</strong>
+					¿Estás esperando a tu próxima celebración? Pues no esperes más e invéntate un motivo. Ven con tu familia, con tus 
+					amigos, traeros a los niños. Se acabó el ir a unos sitios con los padres o hijos y a otros con los amigos… En merendalia 
+					hay hueco para todos.</br></br></p>
+					<strong>Somos el primer restaurante completamente privado en el centro de Valladolid.</strong></br> </br>
+					¡Os esperamos!
+
+
+					</br>
+					
+
+				</div>
+
+			</div>
+			
+			<!-- enlace de 'leer menos...' para ocultar el texto y las imágenes -->
+			<span class="leerMenos"><strong>leer menos...</strong></span>
+
+		</div>
+
+	</div>
+
+	<div class="articulo" id="que_es_merendalia">
 
 		<!-- PARTE PRINCIPAL DEL ARTÍCULO CON IMAGEN GRANDE Y TEXTO -->
 
