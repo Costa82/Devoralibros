72befad0d8dcaf98d894c4c23bfbd58a876c3293
Costa82 <costa_torito@hotmail.com>
2019-08-21 21:00:50 +0200

    Corrección último blog
    Centrado de textos entre imágenes

--- Merendalia/php/blogMerendalia.php
+++ Merendalia/php/blogMerendalia.php
@@ -86,6 +86,292 @@
 
 	</div>
 	
+	<div class="articulo" id="reservas_espacio">
+
+		<!-- PARTE PRINCIPAL DEL ARTÍCULO CON IMAGEN GRANDE Y TEXTO -->
+
+		<h3 class="titulo_articulo">RESERVA EL ESPACIO EN FERIAS</h3>
+
+		<div class="fechaArticulo">
+			<p>21 de agosto de 2019</p>
+		</div>
+
+		<img
+			src='../img/Portada.jpg'
+			title='amigos fiesta merendalia'
+			alt='amigos fiesta merendalia'
+			class="img_articulo" />
+
+		<div class="explicacion_articulo">
+
+			<p>¡Hola merendalios!</br></br>
+			
+			Hace mucho que no os contábamos nada por aquí y es que han sido meses de mucho trabajo y cambios. Nos ha dado tiempo, entre otras cosas a
+			tener un pequeñín. ¡Hemos sido padres!  Y como estamos muy contentos, se lo decimos a todo el mundo. También tenemos mucho sueño…
+			El verano va dando sus últimos coletazos, y aunque supone la tristeza de muchos, los pucelanos tenemos un AS en la manga, ¡nuestras Ferias!
+			Este año del 6 al 15 de septiembre. Para nosotros además son unas fechas muy importantes porque hará un añito que abrimos nuestras puertas.
+			A lo largo de este año, muchas celebraciones y eventos y también, por qué no decirlo, muchos quebraderos de cabeza. En la balanza más alegrías
+			que penas, gracias a vosotros, a todos los que nos habéis dado la oportunidad de conocernos y nos habéis apoyado a lo largo de este caminito.
+			Y gracias también a los que lo haréis en el próximo año, que seréis muchos más, estamos convencidos.</br></br>
+			Hoy os hablamos de las reservas de espacio:</br>
+ 
+			</p></br>
+			
+			<span class="leerMas"><strong>leer más...</strong></span>
+
+			<!-- Esta parte del texto estará oculta hasta hacer click en 'leer más...' -->
+
+			<div class="texto_leerMas">
+
+				</br>
+				<p>
+					¿Qué celebraciones de espacio se han hecho?</br></br>
+			&nbsp&nbsp&nbsp&nbsp-	Cumpleaños</br>
+			&nbsp&nbsp&nbsp&nbsp-	Fiestas sorpresa</br>
+			&nbsp&nbsp&nbsp&nbsp-	Fiestas de disfraces</br>
+			&nbsp&nbsp&nbsp&nbsp-	Despedidas de solter@s</br>
+			&nbsp&nbsp&nbsp&nbsp-	Aniversarios de bodas</br>
+			&nbsp&nbsp&nbsp&nbsp-	Jubilaciones</br>
+			&nbsp&nbsp&nbsp&nbsp-	Baby Shower</br>
+			&nbsp&nbsp&nbsp&nbsp-	Bautizos</br>
+			&nbsp&nbsp&nbsp&nbsp-	…</br>
+
+				Hoy queremos hacer foco en una celebración muy importante, LA DE PORQUE SÍ. La de porque tengo amigos y familia y me junto con ellos a
+				pasar un buen rato, a merendar, a reír…</br> </br>
+				La mayoría de las celebraciones que hemos mencionado tienen un único anfitrión. En las de porque sí, EL GRUPO ES EL ANFITRIÓN.</br>
+				Con motivo de las ferias, quien más quien menos se junta para subir a las Gastronómicas, tomar unos pinchos en la Feria de Día o a
+				comer/cenar en un restaurante. Pues bien, ¿porque no hacéis algo distinto y os juntáis este año en merendalia?</br></br>
+				¿Por qué reservar en merendalia en las fiestas?</br></br>
+				&nbsp&nbsp&nbsp&nbsp-	Porque se está de lujo. </br>
+				&nbsp&nbsp&nbsp&nbsp-	Porque en fiestas todo está hasta arriba y en merendalia estaréis con los que queréis estar.</br> 
+				&nbsp&nbsp&nbsp&nbsp-	Porque hemos pensado en los niños con nuestro parque de bolas.</br>
+				&nbsp&nbsp&nbsp&nbsp-	Porque la comida está buenísima. </br> </br>
+				Por 20€/persona * os sorprenderéis de la fiesta que os podemos preparar incluyendo comida, bebida y reserva del espacio.
+				Llamadnos al teléfono 680 21 97 94 o escribid a info@merendalia.es y os informaremos de todo. </br> </br>
+				Venid por la tarde/noche o mejor al medio dia que la gente está mas fresca. ¿Te juntas con los del trabajo un día para tomar unos pinchos?
+				Hazlo diferente y organizad un vermú informal.</br>
+				
+
+				Es muy fácil, reserváis las horas, nos encargáis la comida y solo os queda disfrutar. Bueno… y decidir si las croquetas serán
+				de bacalao o de jamón y la empanada de carne o de atún, por ejemplo. ¡Mejor las dos y así las probáis…!
+					
+				</p>
+
+				<div class="contenedor_articulo_secundario">
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Croquetas.jpg"><img
+							src="../img/Croquetas.jpg"
+							class="img_articulo_secundaria"
+							title="Croquetas de nuestro catering"
+							alt="Croquetas de nuestro catering" /></a>
+						<p class="pieFoto">Croquetas de jamón y bacalao</p>
+
+					</div>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Empanada.jpg"><img
+							src="../img/Empanada.jpg"
+							class="img_articulo_secundaria"
+							title="Empanada ¡casera casera!"
+							alt="Empanada de atún" /></a>
+						<p class="pieFoto">Empanada de atún, ¡rica rica!</p>
+
+					</div>
+					
+					<div class="explicacion_final">
+						<p>La bebida es por consumo. Nosotros os dejamos las cervezas, los refrescos, el vino, el agua… todo en su punto de temperatura,
+						y al finalizar se hace la cuenta de lo que se ha consumido. ¿A que es muy fácil?
+						</p>
+					</div>
+					</br>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Cubilete cervezas.jpg"><img
+							src="../img/Cubilete cervezas.jpg"
+							class="img_articulo_secundaria"
+							title="Cubo de cervezas frías"
+							alt="Cubo de cervezas frías" /></a>
+						<p class="pieFoto">De la nevera al cubo, ¡heladas!</p>
+
+					</div>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Vinoteca.jpg"><img
+							src="../img/Vinoteca.jpg"
+							class="img_articulo_secundaria"
+							title="Vinoteca"
+							alt="Vinoteca" /></a>
+						<p class="pieFoto">Vinoteca</p>
+
+					</div>
+
+					<div class="explicacion_final">
+						<p>¿Sabéis que nuestras mini hamburguesas tienen más de 10 ingredientes y todos ellos, como en todo lo que hacemos, son naturales?
+						</p></br>
+					</div>
+					
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Mini hamburguesas.jpg"><img
+							src="../img/Mini hamburguesas.jpg"
+							class="img_articulo_secundaria"
+							title="Mini hamburguesas"
+							alt="Mini hamburguesas" /></a>
+						<p class="pieFoto">Mini hamburguesas</p>
+
+					</div>
+
+					<div class="explicacion_final">
+						<p>Aquí tenéis un ejemplo de cómo queda la mesa con algunos de nuestros productos. Canapés, tortillas, queso, empanada…
+						Es parte de lo que os encontraréis al entrar.</p></br>
+					</div>
+					
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Canapés miga pan.png"><img
+							src="../img/Canapés miga pan.png"
+							class="img_articulo_secundaria"
+							title="Bandejas de canapés base miga de pan"
+							alt="Bandejas de canapés base miga de pan y volovanes" /></a>
+						<p class="pieFoto">Bandejas de canapés base miga de pan y volovanes</p>
+
+					</div>
+					
+					<div class="explicacion_final">
+						<p>Y de postre, un mousse de limón con frutos rojos, ¡o lo que os apetezca!, una de nuestras tartas mas exitosas la tarta de zanahoria.
+						</p></br>
+					</div>
+
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Postre.jpg"><img
+							src="../img/Postre.jpg"
+							class="img_articulo_secundaria"
+							title="Mousse de limón con frutos rojos"
+							alt="Mousse de limón con frutos rojos" /></a>
+						<p class="pieFoto">Mousse de limón con frutos rojos</p>
+
+					</div>
+					
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Tarta de zanahoria.jpg"><img
+							src="../img/Tarta de zanahoria.jpg"
+							class="img_articulo_secundaria"
+							title="Tarta de zanahoria"
+							alt="Tarta de zanahoria" /></a>
+						<p class="pieFoto">Tarta de zanahoria</p>
+
+					</div>
+
+					<div class="explicacion_final">
+						<p>Y si sois de esas personas que si no tomáis café es como que no habéis comido, pues ¡café al canto!
+						</p></br>
+					</div>
+					
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Maquina de café.jpg"><img
+							src="../img/Maquina de café.jpg"
+							class="img_articulo_secundaria"
+							title="Café y menaje"
+							alt="Café y menaje" /></a>
+						<p class="pieFoto">Café y menaje</p>
+
+					</div>
+					
+					<div class="explicacion_final">
+						<p>¡Venga! y después de estas fotos que abren el apetito, nos despedimos con unas de caritas sonrientes en alguna
+						de las celebraciones que os proponemos.
+						</p></br>
+					</div>
+					
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Gente.jpg"><img
+							src="../img/Gente.jpg"
+							class="img_articulo_secundaria"
+							title="Fiesta en merendalia"
+							alt="Fiesta celebración en merendalia" /></a>
+						<p class="pieFoto"></p>
+
+					</div></br>
+					
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Gente 2.png"><img
+							src="../img/Gente 2.png"
+							class="img_articulo_secundaria"
+							title="Celebración en merendalia"
+							alt="Celebración en merendalia" /></a>
+						<p class="pieFoto"></p>
+
+					</div></br>
+					
+					<div class="articulo_secundario">
+
+						<!-- img con zoom -->
+						<a class="fancybox" rel="group"
+							href="../img/Vaquero.png"><img
+							src="../img/Vaquero.png"
+							class="img_articulo_secundaria"
+							title="Celebración en merendalia"
+							alt="Celebración en merendalia" /></a>
+						<p class="pieFoto"></p>
+
+					</div>
+					
+					<div class="explicacion_final">
+						<p>• Nota: El precio es una aproximación, dependerá del día de la semana, las horas que dure la
+						reserva, el número de personas, así como de la comida contratada.
+						</p></br>
+					</div>
+					
+					</br>
+					
+
+				</div>
+
+			</div>
+			
+			<!-- enlace de 'leer menos...' para ocultar el texto y las imágenes -->
+			<span class="leerMenos"><strong>leer menos...</strong></span>
+
+		</div>
+
+	</div>
+	
+	
+	
+	
+	
 	<div class="articulo" id="menús_privados">
 
 		<!-- PARTE PRINCIPAL DEL ARTÍCULO CON IMAGEN GRANDE Y TEXTO -->
